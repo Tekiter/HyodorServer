c22c4d0dd520538b53c023222a4466168ecbf443
TEKITER-PC <tekiter8@gmail.com>
2019-08-22 00:41:31 +0900

    Add error output

--- server/routes.py
+++ server/routes.py
@@ -10,38 +10,42 @@ CACHE_PATH = os.path.join(FILE_PATH, "apicache")
 
 @app.route('/apicache/<path:path>')
 def apicache(path):
-    resulttext = ""
-    resultstatus = 500
-    
-    cachefile = hashlib.sha256(path.encode()).hexdigest() + ".cache"
-    cachefile = os.path.join(CACHE_PATH, cachefile)
-    
+    try:
 
-    if os.path.isfile(cachefile):
-        cachetime = datetime.datetime.fromtimestamp(os.path.getmtime(cachefile))
+        resulttext = ""
+        resultstatus = 500
+        
+        cachefile = hashlib.sha256(path.encode()).hexdigest() + ".cache"
+        cachefile = os.path.join(CACHE_PATH, cachefile)
+        
 
-        if cachetime > datetime.datetime.now() - datetime.timedelta(days=1):
-            with open(cachefile, 'r') as fp:
-                resulttext = fp.read()
-                resultstatus = 200
-                res = Response(response=resulttext, status=resultstatus, mimetype="application/xml")
-                res.headers["Content-Type"] = "text/xml; charset=utf-8"
-                return res
+        if os.path.isfile(cachefile):
+            cachetime = datetime.datetime.fromtimestamp(os.path.getmtime(cachefile))
 
-    
+            if cachetime > datetime.datetime.now() - datetime.timedelta(days=1):
+                with open(cachefile, 'r') as fp:
+                    resulttext = fp.read()
+                    resultstatus = 200
+                    res = Response(response=resulttext, status=resultstatus, mimetype="application/xml")
+                    res.headers["Content-Type"] = "text/xml; charset=utf-8"
+                    return res
 
-    r = requests.get("http://api.korea.go.kr/openapi/" + path)
-    resulttext = r.text
-    resultstatus = r.status_code
+        
+
+        r = requests.get("http://api.korea.go.kr/openapi/" + path)
+        resulttext = r.text
+        resultstatus = r.status_code
 
 
-    with open(cachefile, "w") as fp:
-        fp.write(resulttext)
-    
+        with open(cachefile, "w") as fp:
+            fp.write(resulttext)
+        
 
-    res = Response(response=resulttext, status=resultstatus, mimetype="application/xml")
-    res.headers["Content-Type"] = "text/xml; charset=utf-8"
-    return res
+        res = Response(response=resulttext, status=resultstatus, mimetype="application/xml")
+        res.headers["Content-Type"] = "text/xml; charset=utf-8"
+        return res
+    except Exception as err:
+        return str(err), 500
 
 
 @app.route('/', defaults={'path': ''})
