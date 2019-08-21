92d437035c69ac4ad512d719e8dd4fe00e1497bf
TEKITER-PC <tekiter8@gmail.com>
2019-08-22 00:05:03 +0900

    Create cache server

--- server/routes.py
+++ server/routes.py
@@ -1,5 +1,47 @@
-from flask import jsonify, render_template
+import os, datetime
+import requests
+import hashlib
+
+from flask import jsonify, render_template, Response
 from flask import current_app as app
+
+FILE_PATH = os.path.dirname(os.path.abspath(__file__))
+CACHE_PATH = os.path.join(FILE_PATH, "apicache")
+
+@app.route('/apicache/<path:path>')
+def apicache(path):
+    resulttext = ""
+    resultstatus = 500
+    
+    cachefile = hashlib.sha256(path.encode()).hexdigest() + ".cache"
+    cachefile = os.path.join(CACHE_PATH, cachefile)
+    
+
+    if os.path.isfile(cachefile):
+        cachetime = datetime.datetime.fromtimestamp(os.path.getmtime(cachefile))
+
+        if cachetime > datetime.datetime.now() - datetime.timedelta(days=1):
+            with open(cachefile, 'r') as fp:
+                resulttext = fp.read()
+                resultstatus = 200
+                res = Response(response=resulttext, status=resultstatus, mimetype="application/xml")
+                res.headers["Content-Type"] = "text/xml; charset=utf-8"
+                return res
+
+    
+
+    r = requests.get("http://api.korea.go.kr/openapi/" + path)
+    resulttext = r.text
+    resultstatus = r.status_code
+
+
+    with open(cachefile, "w") as fp:
+        fp.write(resulttext)
+    
+
+    res = Response(response=resulttext, status=resultstatus, mimetype="application/xml")
+    res.headers["Content-Type"] = "text/xml; charset=utf-8"
+    return res
 
 
 @app.route('/', defaults={'path': ''})
@@ -7,7 +49,3 @@ from flask import current_app as app
 def main_page(path):
     return render_template('index.html')
 
-# @app.route('/', defaults={'path': ''})
-# @app.route('/<path:path>')
-# def asdf(path):
-#     return str(path)
